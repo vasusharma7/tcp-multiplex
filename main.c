10a6f78d13d244f3dcce8668a3c011750e218086
bhrigu77 <bhrigusharma777@gmail.com>
2024-10-06 03:07:46 +0530

    test: Added test code for connection establishment

--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 #include <netinet/ip.h>	/* needed for sockaddr_in */
 #include <arpa/inet.h> /* needed for converting presentational addresses to network format. */
 #include <sys/types.h>
+#include <stdio.h>
+#include <netdb.h>      /* for gethostbyname() */
+#include <unistd.h>  
 
 #if !defined(IP_FREEBIND)
 #define IP_FREEBIND 15
@@ -15,14 +18,17 @@
 #define SOL_IP 0
 #endif /* !SOL_IP */
 
+
+int connection_test(int);
+
 void paddr(unsigned char* a) {
     printf("%d.%d.%d.%d\n", a[0], a[1], a[2], a[3]);
 }
 
 int main() {
-    int i = 0, j = 1, k = 0;
+    int i = 0, j = 63, k = 0;
     char ip[16];
-    while (i < 65536) {
+    while (i < 1) {
         int sockfd, portno;
         if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
             perror("cannot create socket");
@@ -69,8 +75,44 @@ int main() {
         printf("i = %d | IP: %s, port: %d\n", i, ip, portno);
 
         i += 1;
+
+        connection_test(sockfd);
     }
 
     return 0;
 
+}
+
+int connection_test(int fd) {
+    // char *host = "example.com";
+    // struct hostent *hp;     /* host information */
+    // /* look up the address of the server given its name */
+    // hp = gethostbyname(host);
+    // if (!hp) {
+    //     fprintf(stderr, "could not obtain address of %s\n", host);
+    //     return 0;
+    // } else {
+    //     printf("IP Address of Host: ");
+    //     paddr(hp->h_addr_list[0]);
+    // }
+
+    struct sockaddr_in servaddr;    /* server address */
+    /* fill in the server's address and data */
+    memset((char*)&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(8080);
+
+
+
+    inet_pton(AF_INET, "192.168.51.63", &servaddr.sin_addr);
+
+    // /* put the host's address into the server address structure */
+    // memcpy((void *)&servaddr.sin_addr,hp->h_addr_list[0], hp->h_length);
+
+    /* connect to server */
+    if (connect(fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("connect failed");
+        return 0;
+    }
+    write(fd,"Hello Server",12);
 }
